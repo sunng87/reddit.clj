baf731a8861176d06318ff06567e528266f9a9fc
Sun Ning <classicning@gmail.com>
2011-07-22 23:04:07 +0800

    add clojar plugin to project.clj

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "A reddit API wrapper for clojure"
   :dependencies [[org.clojure/clojure "1.2.1"], 
                  [clj-http "0.1.3"],
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
