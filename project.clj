ac090e4486bdb0b4b53b1d23644dad0ca86ec396
Sunng <classicning@gmail.com>
2011-07-25 16:00:48 +0800

    add docs

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :dependencies [[org.clojure/clojure "1.2.1"], 
                  [clj-http "0.1.3"],
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[lein-clojars "0.6.0"]])
+  :dev-dependencies [[lein-clojars "0.6.0"]
+                     [org.clojars.weavejester/autodoc "0.9.0"
+                       :exclusions [
+                         org.clojure/clojure-contrib
+                         org.clojure/clojure]]])
