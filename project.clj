3aaa9cb4a68283efb39a2627e4482f846289269e
Sun Ning <classicning@gmail.com>
2011-10-27 19:41:38 +0800

    bump version for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reddit.clj "0.3.1-SNAPSHOT"
+(defproject reddit.clj "0.3.1"
   :description "A reddit API wrapper for clojure"
   :author "Sun Ning <classicning@gmail.com>"
   :dependencies [[org.clojure/clojure "1.2.1"], 
