d0205eb227e94dc951a61607647551cbfbe766aa
Sunng <classicning@gmail.com>
2012-01-06 15:40:48 +0800

    moving to clojure 1.3

--- project.clj
+++ project.clj
@@ -1,11 +1,8 @@
-(defproject reddit.clj "0.3.1"
+(defproject reddit.clj "0.3.2"
   :description "A reddit API wrapper for clojure"
   :author "Sun Ning <classicning@gmail.com>"
-  :dependencies [[org.clojure/clojure "1.2.1"], 
-                 [clj-http "0.2.1"],
+  :dependencies [[org.clojure/clojure "1.3.0"], 
+                 [clj-http "0.2.7"],
                  [org.clojure/data.json "0.1.1"]]
   :dev-dependencies [[lein-clojars "0.6.0"]
-                     [org.clojars.weavejester/autodoc "0.9.0"
-                       :exclusions [
-                         org.clojure/clojure-contrib
-                         org.clojure/clojure]]])
+                     [lein-autodoc "0.9.0"]])
