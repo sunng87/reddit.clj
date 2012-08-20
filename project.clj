76d22bc3e8fac0fcc4570484f986ab2e81ae12cf
Sun Ning <classicning@gmail.com>
2012-08-20 21:50:59 +0800

    update dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject reddit.clj "0.3.3"
+(defproject reddit.clj "0.3.4"
   :description "A reddit API wrapper for clojure"
   :author "Sun Ning <classicning@gmail.com>"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-http "0.3.1"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-http "0.3.6"]]
   :dev-dependencies [[lein-clojars "0.6.0"]
                      [lein-autodoc "0.9.0"]])
