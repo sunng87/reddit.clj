1d6f30c4e240892835ccb0e0aaea843abda6022f
Sun Ning <nsun@avos.com>
2012-10-08 10:41:31 +0800

    bump to 0.4.0

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ A reddit api wrapper for clojure
 Declare reddit.clj in your project.clj
 
     (defproject xxxx "1.0.0-SNAPSHOT"
-      :dependencies [[reddit.clj "0.3.5"]])
+      :dependencies [[reddit.clj "0.4.0"]])
 
 Use reddit.clj in your clojure code:
 
--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject reddit.clj "0.3.5"
+(defproject reddit.clj "0.4.0"
   :description "A reddit API wrapper for clojure"
   :author "Sun Ning <classicning@gmail.com>"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clj-http "0.3.6"]]
+                 [clj-http "0.5.5"]]
   :dev-dependencies [[lein-clojars "0.6.0"]
                      [lein-autodoc "0.9.0"]])
