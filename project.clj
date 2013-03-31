9ac3aaca34bd8eb3358079b937034e27ec1afd4a
Sun Ning <nsun@avos.com>
2013-03-31 16:27:55 +0800

    release 0.4.1, bugfix

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject reddit.clj "0.4.0"
+(defproject reddit.clj "0.4.1"
   :description "A reddit API wrapper for clojure"
   :author "Sun Ning <classicning@gmail.com>"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clj-http "0.5.5"]]
+                 [clj-http "0.6.5"]]
   :dev-dependencies [[lein-clojars "0.6.0"]
                      [lein-autodoc "0.9.0"]])
